6893f3a79f6f7352f9e5d3834a93c18f05469a06
xylous <xylous.e@gmail.com>
2021-01-27 17:00:33 +0200

    Wrap code in a function, getting rid of global variables

--- mzpm.zsh
+++ mzpm.zsh
@@ -3,18 +3,22 @@
 [[ -d ${PLUGIN_DIR} ]] || mkdir ${ZPM_PLUGIN_DIR}
 
 # Download not already downloaded plugins and source them
-for plugin in $plugins; do
-    local plug_name=$(basename ${plugin})
-    local plug_location=${PLUGIN_DIR}/${plug_name}
+load_zsh_plugins() {
+    for plugin in $plugins; do
+        local plug_name=$(basename ${plugin})
+        local plug_location=${PLUGIN_DIR}/${plug_name}
 
-    if [[ ! -d ${plug_location} ]]; then
-        local plug_link="https://github.com/${plugin}"
-        git clone ${plug_link} ${plug_location} &>/dev/null
-    fi
+        if [[ ! -d ${plug_location} ]]; then
+            local plug_link="https://github.com/${plugin}"
+            git clone ${plug_link} ${plug_location} &>/dev/null
+        fi
 
-    if [[ -f ${plug_location}/${plug_name}.plugin.zsh ]]; then
-        source ${plug_location}/${plug_name}.plugin.zsh
-    elif [[ -f ${plug_location}/${plug_name}.zsh ]]; then
-        source ${plug_location}/${plug_name}.zsh
-    fi
-done
+        if [[ -f ${plug_location}/${plug_name}.plugin.zsh ]]; then
+            source ${plug_location}/${plug_name}.plugin.zsh
+        elif [[ -f ${plug_location}/${plug_name}.zsh ]]; then
+            source ${plug_location}/${plug_name}.zsh
+        fi
+    done
+}
+
+load_zsh_plugins
